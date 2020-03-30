c5b633ecc0eb5cd0689e77c8d344a42af42f07ae
diana134 <diana@dianacarrier.com>
2020-03-29 21:40:09 -0500

    make a request

--- ravelry-api-fun.js
+++ ravelry-api-fun.js
@@ -1,1 +1,8 @@
-console.log("hi");
+const Http = new XMLHttpRequest();
+const url='https://api.ravelry.com/current_user.json';
+Http.open("GET", url);
+Http.send();
+
+Http.onreadystatechange = (e) => {
+  console.log(Http.responseText)
+}
